2ac6a76ff4d2c10a2cfa96019bae165eedec4a6d
closure <retval@me.com>
2014-07-02 23:39:24 +0800

    Initial Commit

---
+++ PerceptualHashAlgorithm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PerceptualHashAlgorithm
+//
+//  Created by closure on 7/2/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PerceptualHashAlgorithm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PerceptualHashAlgorithm
+//
+//  Created by closure on 7/2/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
